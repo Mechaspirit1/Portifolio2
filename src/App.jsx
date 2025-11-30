311e933282d20b67bdd4c7da7b14cffdf47d6c64
pablo <pabloloschi200@gmail.com>
2025-11-30 15:02:23 -0300

    fixing pathing

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,21 @@ function App() {
                 <h1 className="title"> Projects</h1>
                 <div className="cardBody">
                     <Cards
-                        imgSrc = "/weatherly.png"
+                        imgSrc = "./weatherly.png"
                         cardTitle = "Weatherly.js"
                         cardText = {"A fully functioning front-end weather application\nusing plain HTML, CSS and Javascript.\n"}
                         projLink = "https://mechaspirit1.github.io/Weather-App/"
                         srcCode = "https://github.com/Mechaspirit1/Weather-App"
                     />
                     <Cards
-                        imgSrc = "/book.png"
+                        imgSrc = "./book.png"
                         cardTitle = "Book Repo"
                         cardText = {"A front-end application to search for books and authors\nmaking use of the Open Library API\n"}
                         projLink = "https://mechaspirit1.github.io/Book-Repo/"
                         srcCode = "https://github.com/Mechaspirit1/Book-Repo"
                     />
                     <Cards
-                        imgSrc = "/palette.png"
+                        imgSrc = "./palette.png"
                         cardTitle = "Color Palette Generator"
                         cardText = {"A basic application demonstrating DOM manipulation\nand handling random values in code\n"}
                         projLink = "https://mechaspirit1.github.io/Color-palette-generator/"
