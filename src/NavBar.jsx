fb38e764d4242ba5bb749307b75ae4c8dc18923b
pablo <pabloloschi200@gmail.com>
2025-11-29 22:13:38 -0300

    deploy configuration test

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,12 +7,16 @@ function Navbar(){
             <h2>Pablo Loschi</h2>
 
             <div className="links">
-                <a href="https://github.com/Mechaspirit1" target="_blank">Github</a>
-                <a href="https://www.linkedin.com/in/pablo-loschi-b55055381/" target="_blank">LinkedIn</a>
-                <a href="https://github.com/Mechaspirit1" target="_blank">CV</a>
+                <form action="https://github.com/Mechaspirit1" target="_blank">
+                    <button className="btn">Github</button>
+                </form>
+                <form action="https://www.linkedin.com/in/pablo-loschi-b55055381/" target="_blank">
+                    <button className="btn">LinkedIn</button>
+                </form>
+                <form action="https://github.com/Mechaspirit1" target="_blank">
+                    <button className="btn">CV</button>
+                </form>
             </div>
-
-            <Button/>
         </>
     );
 }
